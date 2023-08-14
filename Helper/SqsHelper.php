789b20934178790cc1e6a9758b509f1a0cb2e6ed
Simon CARRE <simoncarre54@gmail.com>
2023-08-14 16:10:11 +0200

    Add sqs helper

---
+++ Helper/SqsHelper.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Autobus\Bundle\BusBundle\Helper;
+
+use Aws\Exception\AwsException;
+use Aws\Sqs\SqsClient;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * AWS SQS helper
+ *
+ * @author  Simon CARRE <simon.carre@clickandmortar.fr>
+ * @package Autobus\Bundle\BusBundle\Helper
+ */
+class SqsHelper
+{
+    /**
+     * AWS SQS Version
+     *
+     * @var string
+     */
+    const SQS_VERSION = '2012-11-05';
+
+    /**
+     * @var  SqsClient
+     */
+    protected $sqsClient;
+
+    /**
+     *  SqsHelper constructor.
+     */
+    public function __construct()
+    {
+        $this->sqsClient = new SqsClient([
+            'region'  => getenv('AWS_REGION'),
+            'version' => self::SQS_VERSION
+        ]);
+    }
+
+    /**
+     * Get queue url by $queueName
+     *
+     * @param string $queueName
+     *
+     * @return string | null
+     */
+    public function getQueueUrlByName($queueName)
+    {
+        try {
+            $result = $this->sqsClient->getQueueUrl([
+                'QueueName' => $queueName,
+            ]);
+            return $result->get('QueueUrl');
+        } catch (AwsException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * Create queue and return url
+     *
+     * @param string $queueName
+     *
+     * @return string | null
+     */
+    public function createQueue($queueName)
+    {
+        try {
+            $result = $this->sqsClient->createQueue([
+                'QueueName' => $queueName,
+            ]);
+            return $result->get('QueueUrl');
+        } catch (AwsException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * Write $message in $queueUrl
+     *
+     * @param string $queueUrl
+     * @param string $message
+     *
+     * @return bool
+     */
+    public function writeMessage($queueUrl, $message)
+    {
+        try {
+            $this->sqsClient->sendMessage([
+                'QueueUrl' => $queueUrl,
+                'MessageBody' => $message
+            ]);
+            return true;
+        } catch (AwsException $e) {
+            return false;
+        }
+    }
+}
--- Queue/SqsWriter.php
+++ Queue/SqsWriter.php
@@ -2,9 +2,8 @@
 
 namespace Autobus\Bundle\BusBundle\Queue;
 
+use Autobus\Bundle\BusBundle\Helper\SqsHelper;
 use Autobus\Bundle\BusBundle\Helper\TopicHelper;
-use Aws\Exception\AwsException;
-use Aws\Sqs\SqsClient;
 
 /**
  * AWS SQS writer
@@ -15,30 +14,25 @@ use Aws\Sqs\SqsClient;
 class SqsWriter implements WriterInterface
 {
     /**
-     * AWS SQS Version
-     *
-     * @var string
-     */
-    const SQS_VERSION = '2012-11-05';
-
-    /**
-     * @var  SqsClient
-     */
-    protected $sqsClient;
-
-    /**
      * @var TopicHelper
      */
     protected $topicHelper;
 
     /**
+     * @var  SqsHelper
+     */
+    protected $sqsHelper;
+
+    /**
      * Writer constructor.
      *
      * @param TopicHelper $topicHelper
+     * @param SqsHelper   $sqsHelper
      */
-    public function __construct(TopicHelper $topicHelper)
+    public function __construct(TopicHelper $topicHelper, SqsHelper $sqsHelper)
     {
         $this->topicHelper = $topicHelper;
+        $this->sqsHelper   = $sqsHelper;
     }
 
     /**
@@ -46,20 +40,16 @@ class SqsWriter implements WriterInterface
      */
     public function write($topicName, $message)
     {
-        $this->sqsClient = new SqsClient([
-            'region'  => getenv('AWS_REGION'),
-            'version' => self::SQS_VERSION
-        ]);
-        $queueName       = $this->topicHelper->getRealTopicName($topicName);
+        $queueName = $this->topicHelper->getRealTopicName($topicName);
 
         // Create / get queue
-        $queueUrl = $this->getQueueUrlByName($queueName);
+        $queueUrl = $this->sqsHelper->getQueueUrlByName($queueName);
         if ($queueUrl === null) {
-            $queueUrl = $this->createQueue($queueName);
+            $queueUrl = $this->sqsHelper->createQueue($queueName);
         }
 
         // Write message to queue
-        $this->writeMessage($queueUrl, $message);
+        $this->sqsHelper->writeMessage($queueUrl, $message);
     }
 
     /**
@@ -73,64 +63,5 @@ class SqsWriter implements WriterInterface
     public function writeAsArray($topicName, $data)
     {
         $this->write($topicName, json_encode($data));
-    }
-
-    /**
-     * Get queue url by $queueName
-     *
-     * @param string $queueName
-     *
-     * @return string | null
-     */
-    protected function getQueueUrlByName($queueName)
-    {
-        try {
-            $result = $this->sqsClient->getQueueUrl([
-                'QueueName' => $queueName,
-            ]);
-            return $result->get('QueueUrl');
-        } catch (AwsException $e) {
-            return null;
-        }
-    }
-
-    /**
-     * Create queue and return url
-     *
-     * @param string $queueName
-     *
-     * @return string | null
-     */
-    protected function createQueue($queueName)
-    {
-        try {
-            $result = $this->sqsClient->createQueue([
-                'QueueName' => $queueName,
-            ]);
-            return $result->get('QueueUrl');
-        } catch (AwsException $e) {
-            return null;
-        }
-    }
-
-    /**
-     * Write $message in $queueUrl
-     *
-     * @param string $queueUrl
-     * @param string $message
-     *
-     * @return bool
-     */
-    protected function writeMessage($queueUrl, $message)
-    {
-        try {
-            $this->sqsClient->sendMessage([
-                'QueueUrl' => $queueUrl,
-                'MessageBody' => $message
-            ]);
-            return true;
-        } catch (AwsException $e) {
-            return false;
-        }
     }
 }
